4f2c5cef3c09bc0dc5295c753e6b8621c8f27de7
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-02 14:47:51 +0300

    navbar - login css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
+    <div className={"App font-black text-3xl mx-auto w-1/3 min-w-fit my-4"}>
+      <header className="flex border-b-4 border-black">
+        <nav className="flex flex-row mb-4">
+          <span className="p-6">
+            <h1>FRIENDS DATABASE</h1>
+          </span>
+
+          <ul className="flex flex-row text-white gap-5">
+            <li className=" p-6 bg-black">LOGIN.</li>
+            <li className=" p-6 bg-black">FRIENDLIST.</li>
+            <li className=" p-6 bg-black">ADDFRIEND.</li>
+            <li className=" p-6 bg-black">LOGUT</li>
+          </ul>
+        </nav>
+      </header>
       <Login></Login>
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,15 @@
 export default function Login() {
   return (
-    <div className={"font-black text-3xl mx-auto w-1/3 min-w-fit"}>
-      <h2 className="text-8xl ">LOGIN</h2>
+    <div className={"font-black text-3xl mx-auto w-1/2 min-w-fit mt-20"}>
+      <h2 className="text-8xl mb-5 ">LOGIN</h2>
       <form className={"flex flex-col gap-3"}>
         <label className={"flex flex-col"}>
           <span className="flex self-start"> USERNAME</span>{" "}
-          <input className="bg-black h-24"></input>{" "}
+          <input className="bg-black h-24  text-white"></input>{" "}
         </label>
         <label className={"flex flex-col "}>
           <span className="flex self-start"> PASSWORD</span>{" "}
-          <input className="bg-black  h-24"></input>{" "}
+          <input className="bg-black  h-24  text-white"></input>{" "}
         </label>
         <button className="bg-black h-24 mt-5 text-white">SUBMIT</button>
       </form>
