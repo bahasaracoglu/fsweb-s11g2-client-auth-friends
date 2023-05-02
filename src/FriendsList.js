ae17068c405b5d4893f864983bae82ac8521d081
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-02 17:35:13 +0300

    liste geliyor.

--- src/FriendsList.js
+++ src/FriendsList.js
@@ -1,12 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function FriendsList() {
-  const list = [{ name: "BAHA", mail: "BAHA@ASDASD.COM" }];
+  const [list, setList] = useState([]);
+  //const list = [{ name: "BAHA", mail: "BAHA@ASDASD.COM" }];
+
+  const myToken = localStorage.getItem("friends_token");
+
+  const auth = {
+    headers: {
+      Authorization: myToken,
+    },
+  };
+
+  console.log(myToken);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/friends", auth)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res);
+
+          setList(res.data);
+          //history.push("/friends");
+        }
+      })
+      .catch((error) => console.log(error.response));
+  }, []);
 
   return (
     <div className={"font-black text-3xl mx-auto w-1/2 min-w-fit mt-20"}>
       <h2 className="text-8xl mb-5 ">FRIENDS LIST</h2>
-      <ul>
+      <ul className="text-left">
         {list.map((char, i) => (
-          <li key={i}>{`-${char.name} - ${char.mail}`}</li>
+          <li
+            key={i}
+            className="mb-4"
+          >{`-${char.name.toUpperCase()} - ${char.email.toUpperCase()}`}</li>
         ))}
       </ul>
     </div>
