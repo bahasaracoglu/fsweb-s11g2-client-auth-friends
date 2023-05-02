313018b7a6079469846b012940e667b9a7b18680
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-02 18:09:10 +0300

    addFriend çalışıyor.

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -1,5 +1,8 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
+import { axiosWithAuth } from "./axiosAuth";
+import { nanoid } from "nanoid";
 
 export default function AddFriend() {
   const {
@@ -10,27 +13,52 @@ export default function AddFriend() {
 
   const history = useHistory();
 
-  function onSubmit(data) {}
+  //api/friends
+
+  const randomAge = () => {
+    return Math.floor(75 * Math.random());
+  };
+
+  function onSubmit(data) {
+    const updatedData = {
+      ...data,
+      id: nanoid(),
+      age: randomAge(),
+    };
+
+    console.log(updatedData);
+
+    axiosWithAuth()
+      .post("http://localhost:9000/api/friends", updatedData)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res);
+
+          //history.push("/friends");
+        }
+      })
+      .catch((error) => console.log(error.response));
+  }
 
   return (
     <div className={"font-black text-3xl mx-auto w-1/2 min-w-fit mt-20"}>
       <h2 className="text-8xl mb-5 ">ADD FRIEND</h2>
       <form className={"flex flex-col gap-3"} onSubmit={handleSubmit(onSubmit)}>
-        <label className={"flex flex-col"}>
+        <label className={"flex flex-col mt-2"}>
           <span className="flex self-start"> FRIEND NAME</span>
           <input
             className="bg-black h-24  text-white"
             defaultValue=""
-            {...register("username")}
+            {...register("name")}
           ></input>
         </label>
-        <label className={"flex flex-col mt-1 "}>
-          <span className="flex self-start"> FRIEND MAIL</span>
+        <label className={"flex flex-col mt-2"}>
+          <span className="flex self-start"> FRIEND EMAIL</span>
           <input
             type="email"
             className="bg-black  h-24  text-white"
             defaultValue=""
-            {...register("password")}
+            {...register("mail")}
           ></input>
         </label>
         <button type="submit" className="bg-black h-24 mt-5 text-white">
